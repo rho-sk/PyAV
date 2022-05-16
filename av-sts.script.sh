4b9d0a83ae337aaf532d106a8569340ff4c8a31d
rho <rho@optimaideas.com>
2022-05-16 16:06:34 +0200

    patch 4.1.3 not used - too complicated dist build and clenup

--- av-sts.script.sh
+++ av-sts.script.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+mkdir -p /home/pyav_builder
+cp -r . /home/pyav_builder
+cd /home/pyav_builder
+
 export PYAV_LIBRARY=ffmpeg-4.1.6
 source scripts/activate.sh
 pip3 install --upgrade -r tests/requirements.txt
 make
 python3 setup.py  bdist_wheel
+
+mkdir -p /home/pyav/dist
+cp -f /home/pyav_builder/dist/*.*  /home/pyav/dist
+rm -rf  /home/pyav_builder
 
